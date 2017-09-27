a707a3ec3630fb68170e00a5791ac4d5af33cf7e
hansenjl <jenn.leigh.hansen@gmail.com>
2017-09-27 15:32:20 -0700

    Define destroy and create methods for the problems controller

--- app/controllers/api/problems_controller.rb
+++ app/controllers/api/problems_controller.rb
@@ -1,14 +1,39 @@
 class Api::ProblemsController < ApplicationController
 
+  before_action :set_problem, only: [:show, :destroy]
+
   def index
     render json: Problem.all
   end
 
   def show
-    render json: Problem.find_by(id: params[:id])
+    render json: @problem
   end
 
   def create
+    problem = Problem.new(problem_params)
+    if problem.save
+      render json: problem
+    else
+      render json: {message: problem.errors}, status: 400
+    end
+  end
 
+  def destroy
+    if @problem.destroy
+      render json: {message: "Successfully Destroyed"}, status: 204
+    else
+      render json: {message: "Unable to destroy"}, status: 400
+    end
+  end
+
+  private
+
+  def set_problem
+    @problem = Problem.find_by(id: params[:id])
+  end
+
+  def problem_params
+    params.require(:problem).permit(:difficulty, :category, :tot_voltage, :tot_resistance, :tot_current, loops: [])
   end
 end
