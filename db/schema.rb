4b5f69e79084353ee4ce847560e9accbac208041
hansenjl <jenn.leigh.hansen@gmail.com>
2017-09-27 15:22:28 -0700

    Change column name from type to category for problems table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170927212050) do
+ActiveRecord::Schema.define(version: 20170927222030) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,7 +27,7 @@ ActiveRecord::Schema.define(version: 20170927212050) do
 
   create_table "problems", force: :cascade do |t|
     t.integer "difficulty"
-    t.string "type"
+    t.string "category"
     t.integer "tot_voltage"
     t.integer "tot_resistance"
     t.integer "tot_current"
