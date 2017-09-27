896fb377f97840695a4e15175142534c48b3aac0
hansenjl <jenn.leigh.hansen@gmail.com>
2017-09-27 15:01:20 -0700

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :problems, only: [:create, :show, :index, :destroy]
+  resources :teachers, only: [:create, :show, :destroy, :update]
+  resources :loops, only: [:create]
+
 end
