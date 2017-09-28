324dc79bc736a2845535907d330fe05419a1517c
hansenjl <jenn.leigh.hansen@gmail.com>
2017-09-28 12:04:54 -0700

    Modified problems controller, loop model, and problem model to allow for nest attributes in the problems params

--- app/models/loop.rb
+++ app/models/loop.rb
@@ -1,4 +1,5 @@
 class Loop < ApplicationRecord
   belongs_to :problem
   has_many :resistors
+  accepts_nested_attributes_for :resistors
 end
--- app/models/problem.rb
+++ app/models/problem.rb
@@ -2,4 +2,5 @@ class Problem < ApplicationRecord
   has_many :teacher_problems
   has_many :teachers, through: :teacher_problems
   has_many :loops
+  accepts_nested_attributes_for :loops
 end
