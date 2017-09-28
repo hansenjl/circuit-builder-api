595286ef5bd8ca7c377a2c3bf3634a66ab3f22b0
hansenjl <jenn.leigh.hansen@gmail.com>
2017-09-27 17:05:34 -0700

    Added validations for user model

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -2,4 +2,5 @@ class Teacher < ApplicationRecord
   has_secure_password
   has_many :teacher_problems
   has_many :problems, through: :teacher_problems
+  validates :email, presence: true, uniqueness: true
 end
