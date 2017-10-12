0ab26efe8fe753be01a85a3a9735c0b456721bf5
hansenjl <jenn.leigh.hansen@gmail.com>
2017-10-12 15:34:20 -0700

    Removed unused routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
-  namespace :api do
+  namespace :api, path: '/' do
     resources :problems, only: [:create, :show, :index, :update, :destroy]
   end
 
-  get '/api/login' => 'sessions#create'
-  get 'api/logout' => 'sessions#destroy'
 
 end
